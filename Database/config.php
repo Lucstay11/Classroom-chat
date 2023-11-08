a8a02340b4f1a48344fa003477fedf72dd07d484
Lucstay11 <89875785+Lucstay11@users.noreply.github.com>
2023-11-08 15:33:03 +0100

    Update config.php

--- Database/config.php
+++ Database/config.php
@@ -1,6 +1,6 @@
 <?php
 $config = [
-    "chat_name"=>"Chat Classe 6TTInfo",
+    "chat_name"=>"Chat Classe",
     "backgroundimage"=>"https://cdn.futura-sciences.com/buildsv6/images/largeoriginal/9/4/0/940b22eda6_50170334_code-informatique.jpg",
      "automoderation"=>false
 ];
